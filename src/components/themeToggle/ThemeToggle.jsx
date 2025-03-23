1d7f1f54227298eb6edc9e521f37dab7f7ff9a31
jeramy <jeramy.reid84@gmail.com>
2025-03-23 13:23:09 -0700

    theme enabled across app, colors not chosen

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,26 +1,40 @@
-
-import { useTheme } from "../../context/ThemeContext";
-
+import { useTheme } from '../../context/ThemeContext';
 
 function ThemeToggle() {
   const { isDarkMode, toggleTheme } = useTheme();
 
   return (
     <button
-      className={`theme-switcher-grid ${isDarkMode ? "night-theme" : ""}`}
+      className={`theme-switcher-grid ${isDarkMode ? 'night-theme' : ''}`}
       onClick={toggleTheme}
       aria-label="Switch theme"
     >
       <div className="sun" aria-hidden="true"></div>
       <div className="moon-overlay" aria-hidden="true"></div>
-      <div className="cloud-ball cloud-ball-left" id="ball1" aria-hidden="true"></div>
-      <div className="cloud-ball cloud-ball-middle" id="ball2" aria-hidden="true"></div>
-      <div className="cloud-ball cloud-ball-right" id="ball3" aria-hidden="true"></div>
-      <div className="cloud-ball cloud-ball-top" id="ball4" aria-hidden="true"></div>
-      <div className="star" id="star1" aria-hidden="true"></div>
+      <div
+        className="cloud-ball cloud-ball-left"
+        id="ball1"
+        aria-hidden="true"
+      ></div>
+      <div
+        className="cloud-ball cloud-ball-middle"
+        id="ball2"
+        aria-hidden="true"
+      ></div>
+      <div
+        className="cloud-ball cloud-ball-right"
+        id="ball3"
+        aria-hidden="true"
+      ></div>
+      <div
+        className="cloud-ball cloud-ball-top"
+        id="ball4"
+        aria-hidden="true"
+      ></div>
+      {/* <div className="star" id="star1" aria-hidden="true"></div>
       <div className="star" id="star2" aria-hidden="true"></div>
       <div className="star" id="star3" aria-hidden="true"></div>
-      <div className="star" id="star4" aria-hidden="true"></div>
+      <div className="star" id="star4" aria-hidden="true"></div> */}
     </button>
   );
 }
