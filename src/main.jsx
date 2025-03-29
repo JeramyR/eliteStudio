a3de9bd137be91f14cc3e85aaf75b2fc9c96f31b
jeramy <jeramy.reid84@gmail.com>
2025-03-28 17:31:08 -0700

    fixed helmet compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,15 @@ import { StrictMode } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
